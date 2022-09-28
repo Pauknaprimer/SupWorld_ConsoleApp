a46f424b4daa7bbb922aefa96bce17776b30a9c7
Pauknaprimer <Kostya.pure@yandex.ru>
2022-09-28 21:07:54 +0300

    Добавьте файлы проекта.

---
+++ SupWorld_ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupWorld_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args) // Запускает консоль
+        {
+            Console.WriteLine("Sup, World!"); // Вывод текста на консоль
+            
+            Console.ReadKey(); // Ожидания от пользователя нажатия любой кнопки 
+        }
+    }
+}
